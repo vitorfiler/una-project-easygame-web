6e48b5ff11e1e5ae349dbb58a3a1d4820c1b1962
vitorArpia <vitornunes@arpiatecnologia.com>
2020-04-28 20:05:14 -0300

    atualização de dependências do projeto e correção de URL de login

--- una-project-easygame-web/src/app/main/login/login.component.ts
+++ una-project-easygame-web/src/app/main/login/login.component.ts
@@ -1,19 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { Login } from 'src/app/_models/login';
-import { HttpClient } from '@angular/common/http';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { Login } from "src/app/_models/login";
+import { HttpClient } from "@angular/common/http";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { AppConst } from "src/app/_const/app.const";
 
 @Component({
-    selector: 'login',
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.scss'],
+  selector: "login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   user = new Login();
   falhaLogin: boolean = false;
-
 
   /**
    * Constructor
@@ -24,43 +24,32 @@ export class LoginComponent implements OnInit {
   private http: HttpClient;
   private _formBuilder: FormBuilder;
   private router: Router;
-  constructor( ) {}
-    ngOnInit(): void {
-        throw new Error("Method not implemented.");
-    }
+  constructor() {}
 
-  //get f() { return this.loginForm.controls; }
+  get f() {
+    return this.loginForm.controls;
+  }
 
-//   login(username: String, senha: String): Promise<any> {
-//       const body =
-//           {'username': username, 'password': senha};
+  login(username: String, senha: String): Promise<any> {
+    const body = { username: username, password: senha };
 
-//       let promisse = this.http.post(`http://localhost:8081/auth/login`, body, {observe: 'response'})
-//           .toPromise();
+    let promisse = this.http
+      .post(`${AppConst.URL_API}`, body, { observe: "response" })
+      .toPromise();
 
-//       return promisse
-//           .then(response => {
-//               localStorage.setItem('currentUser', JSON.stringify(this.user)),
-//                   localStorage.setItem('token', response.body['access_token']),
-//                   this.router.navigate(['home']);
-//           })
-//           .catch(
-//               () => this.falhaLogin = true
-//           );
-//   }
+    return promisse
+      .then((response) => {
+        localStorage.setItem("currentUser", JSON.stringify(this.user)),
+          localStorage.setItem("token", response.body["access_token"]),
+          this.router.navigate(["home"]);
+      })
+      .catch(() => (this.falhaLogin = true));
+  }
 
-  // -----------------------------------------------------------------------------------------------------
-  // @ Lifecycle hooks
-  // -----------------------------------------------------------------------------------------------------
-
-  /**
-   * On init
-   */
-//   ngOnInit(): void {
-//       this.loginForm = this._formBuilder.group({
-//           email: ['', [Validators.required]],
-//           password: ['', Validators.required]
-//       });
-//   }
+  ngOnInit(): void {
+    this.loginForm = this._formBuilder.group({
+      email: ["", [Validators.required]],
+      password: ["", Validators.required],
+    });
+  }
 }
-
