5de0b17990cc3f77a2601a6927b0a66cc4760676
vitorfiler <evitorfiler@gmail.com>
2020-05-12 23:13:25 -0300

    ajustes de menu e atualização da chamada do menu no componente principal

--- una-project-easygame-web/src/app/layout/menu/menu.component.ts
+++ una-project-easygame-web/src/app/layout/menu/menu.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent implements OnDestroy {
+export class MenuComponent implements OnDestroy, OnInit{
   mobileQuery: MediaQueryList;
 
   fillerNav = Array.from({length: 4}, (_, i) => `Nav Item ${i + 1}`);
@@ -26,6 +26,14 @@ export class MenuComponent implements OnDestroy {
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
   }
+  ngOnInit(): void {
+    this.user = localStorage.getItem('currentUser');
+    this.username = JSON.parse(this.user);
+    console.log("sd");
+  }
+
+  user: any;
+  username: any;
 
   ngOnDestroy(): void {
     this.mobileQuery.removeListener(this._mobileQueryListener);
